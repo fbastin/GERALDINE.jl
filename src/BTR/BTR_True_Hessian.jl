9ed75f314d7244f966924a8b100fafda3b00cab7
jean_lc <jl_chartrand@hotmail.com>
2018-12-20 15:10:12 -0500

    Update BTR_True_Hessian.jl

--- src/BTR/BTR_True_Hessian.jl
+++ src/BTR/BTR_True_Hessian.jl
@@ -36,16 +36,15 @@ function btr(f::Function, g!::Function, H!::Function, state::BTRState{Matrix}, x
 end
 
 function OPTIM_btr_TH(f::Function, g!::Function, H!::Function, x0::Vector; verbose::Bool = true, 
-                nmax::Int64 = 1000, acc!::Function = (state, acc) -> nothing, 
-                H = Array{Float64, 2}(I, length(x0), length(x0)), epsilon::Float64 = 1e-4)
+                nmax::Int64 = 1000, acc!::Function = (state, acc) -> nothing, epsilon::Float64 = 1e-4)
+        
     
-    state = BTRState(H)
-    state.x = x0
+    state = BTRState(Array{Float64, 2}(I, length(x0), length(x0)))
+    state.x = copy(x0)
     state.iter = 0
     state.g = zeros(length(x0))
     
-    state, accumulator =  btr(f, g!, H!, state, x0,
-        verbose = verbose, nmax = nmax, epsilon = epsilon, 
-        acc!)
+    state, accumulator =  btr(f, g!, H!, state, x0, 
+                verbose = verbose, nmax = nmax, epsilon = epsilon, acc!)
     return state, accumulator
 end
